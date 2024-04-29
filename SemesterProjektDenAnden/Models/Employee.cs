a4bd88b0343c076a1ed91779a20b833b50bd4c96
Freben111 <frederikbengtson0@gmail.com>
2024-04-29 13:02:57 +0200

    Added employee model

---
+++ SemesterProjektDenAnden/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; } 
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string JobTitle { get; set; }
+    }
+}
