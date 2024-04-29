5d9bdface558a44adddb55ee97d6eb0197bc5637
Freben111 <frederikbengtson0@gmail.com>
2024-04-29 14:07:08 +0200

    EmployeeDA + BL interfaces implemented

--- SemesterProjektDenAnden/BusinessLogic/EmployeeBL.cs
+++ SemesterProjektDenAnden/BusinessLogic/EmployeeBL.cs
@@ -1,7 +1,33 @@
-﻿namespace BusinessLogic
-{
-    public class EmployeeBL
-    {
+﻿using DataAcces.Interfaces;
+using Models;
 
+namespace BusinessLogic
+{
+    public class EmployeeBL : IEmployee
+    {
+        public bool CreateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
