11f35644c01fe730b44a59b5436821eccff61d5b
hyldgaard01 <hyldgaardemilie@gmail.com>
2024-04-29 12:59:20 +0200

    Models, Client, Course, Service
    Models added

---
+++ SemesterProjektDenAnden/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Client
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Phone {  get; set; }
+        public string Mail { get; set; }
+        public string ClAddress { get; set; }
+        public bool Subscriber {  get; set; }
+
+    }
+}
---
+++ SemesterProjektDenAnden/Models/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public float Price {  get; set; }
+        public bool PriceHourly { get; set; }
+    }
+}
