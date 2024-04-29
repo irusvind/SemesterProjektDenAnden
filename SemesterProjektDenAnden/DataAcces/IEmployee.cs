956b0846708d92456beeb409fd6388a98fd65f1d
Frederik Vind <irus@live.dk>
2024-04-29 13:55:37 +0200

    Create IEmployee.cs

---
+++ SemesterProjektDenAnden/DataAcces/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces
+{
+    public interface IEmployee
+    {
+        List<Employee> GetAllEmployees();
+        Employee GetEmployee(int id);
+        bool CreateEmployee(Employee employee);
+        bool DeleteEmployee(Employee employee);
+        bool UpdateEmployee(Employee employee);
+
+    }
+}
