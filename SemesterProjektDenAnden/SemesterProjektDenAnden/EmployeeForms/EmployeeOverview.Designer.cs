9919a501d82d59831bb84b5466152d9f48472e63
hyldgaard01 <hyldgaardemilie@gmail.com>
2024-05-02 12:31:11 +0200

    Employeeoverview design

--- SemesterProjektDenAnden/SemesterProjektDenAnden/EmployeeForms/EmployeeOverview.Designer.cs
+++ SemesterProjektDenAnden/SemesterProjektDenAnden/EmployeeForms/EmployeeOverview.Designer.cs
@@ -28,19 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Startsidelbl = new Label();
+            sagerBtn = new Button();
+            employeeBtn = new Button();
+            clientBtn = new Button();
+            toolsBtn = new Button();
             SuspendLayout();
+            // 
+            // Startsidelbl
+            // 
+            Startsidelbl.AutoSize = true;
+            Startsidelbl.Font = new Font("SimSun", 25.875F, FontStyle.Regular, GraphicsUnit.Point);
+            Startsidelbl.Location = new Point(434, 54);
+            Startsidelbl.Name = "Startsidelbl";
+            Startsidelbl.Size = new Size(177, 35);
+            Startsidelbl.TabIndex = 0;
+            Startsidelbl.Text = "Startside";
+            // 
+            // sagerBtn
+            // 
+            sagerBtn.BackColor = Color.Black;
+            sagerBtn.Font = new Font("SimSun", 19.875F, FontStyle.Regular, GraphicsUnit.Point);
+            sagerBtn.ForeColor = Color.White;
+            sagerBtn.Location = new Point(254, 167);
+            sagerBtn.Name = "sagerBtn";
+            sagerBtn.Size = new Size(250, 150);
+            sagerBtn.TabIndex = 1;
+            sagerBtn.Text = "Sager";
+            sagerBtn.UseVisualStyleBackColor = false;
+            // 
+            // employeeBtn
+            // 
+            employeeBtn.BackColor = Color.Black;
+            employeeBtn.Font = new Font("SimSun", 19.875F, FontStyle.Regular, GraphicsUnit.Point);
+            employeeBtn.ForeColor = Color.White;
+            employeeBtn.Location = new Point(254, 356);
+            employeeBtn.Name = "employeeBtn";
+            employeeBtn.Size = new Size(250, 150);
+            employeeBtn.TabIndex = 2;
+            employeeBtn.Text = "Medarbejdere";
+            employeeBtn.UseVisualStyleBackColor = false;
+            // 
+            // clientBtn
+            // 
+            clientBtn.BackColor = Color.Black;
+            clientBtn.ForeColor = Color.White;
+            clientBtn.Location = new Point(550, 356);
+            clientBtn.Name = "clientBtn";
+            clientBtn.Size = new Size(250, 150);
+            clientBtn.TabIndex = 3;
+            clientBtn.Text = "button3";
+            clientBtn.UseVisualStyleBackColor = false;
+            // 
+            // toolsBtn
+            // 
+            toolsBtn.BackColor = Color.Black;
+            toolsBtn.Font = new Font("SimSun", 19.875F, FontStyle.Regular, GraphicsUnit.Point);
+            toolsBtn.ForeColor = Color.White;
+            toolsBtn.Location = new Point(550, 167);
+            toolsBtn.Name = "toolsBtn";
+            toolsBtn.Size = new Size(250, 150);
+            toolsBtn.TabIndex = 4;
+            toolsBtn.Text = "Værktøjer";
+            toolsBtn.UseVisualStyleBackColor = false;
             // 
             // EmployeeOverview
             // 
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
+            BackColor = Color.White;
             ClientSize = new Size(1065, 655);
+            Controls.Add(toolsBtn);
+            Controls.Add(clientBtn);
+            Controls.Add(employeeBtn);
+            Controls.Add(sagerBtn);
+            Controls.Add(Startsidelbl);
             FormBorderStyle = FormBorderStyle.None;
             Name = "EmployeeOverview";
             Text = "EmployeeOverview";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label Startsidelbl;
+        private Button sagerBtn;
+        private Button employeeBtn;
+        private Button clientBtn;
+        private Button toolsBtn;
     }
 }
