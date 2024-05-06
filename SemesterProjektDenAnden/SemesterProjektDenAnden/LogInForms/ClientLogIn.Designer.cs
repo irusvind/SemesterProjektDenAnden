0147aaba0bf50c78a85934c2aa5d840d5b8bcf99
hyldgaard01 <hyldgaardemilie@gmail.com>
2024-05-06 13:30:59 +0200

    Design combo login

--- SemesterProjektDenAnden/SemesterProjektDenAnden/LogInForms/ClientLogIn.Designer.cs
+++ SemesterProjektDenAnden/SemesterProjektDenAnden/LogInForms/ClientLogIn.Designer.cs
@@ -90,7 +90,7 @@
             comboBox1.FormattingEnabled = true;
             comboBox1.Location = new Point(330, 210);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 24);
+            comboBox1.Size = new Size(216, 24);
             comboBox1.TabIndex = 4;
             // 
             // ClientLogIn
