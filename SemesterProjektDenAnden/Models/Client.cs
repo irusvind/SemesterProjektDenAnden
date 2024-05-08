10a014bc63baeb6ab68c140675452c424f7544d5
irusvind <irus@live.dk>
2024-05-08 13:43:00 +0200

    Fixed validator again

--- SemesterProjektDenAnden/Models/Client.cs
+++ SemesterProjektDenAnden/Models/Client.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,19 +12,19 @@ namespace Models
     {
         public int ClientId { get; set; }
 
-        [Required, StringLength(50), Display(Name = "First Name"), RegularExpression(@"^[a-zA-Z]")]
+        [Required, StringLength(50), Display(Name = "First Name"), RegularExpression(@"^[a-zA-Z''-'\s]{1,50}$")]
         public string FirstName { get; set; }
 
-        [Required, StringLength(50), RegularExpression(@"^[a-zA-Z]")]
+        [Required, StringLength(50), RegularExpression(@"^[a-zA-Z''-'\s]{1,50}$")]
         public string LastName { get; set; }
 
-        [Required, Display(Name = "Phone"), RegularExpression(@"^[0-9]")]
+        [Required, Display(Name = "Phone")]
         public int Phone {  get; set; }
 
         [Required,StringLength(50), EmailAddress(ErrorMessage = "Invalid email format"),Display(Name = "Email")]
         public string Mail { get; set; }
 
-        [MaxLength(50), RegularExpression(@"^[a-zA-Z]")]
+        [MaxLength(50), RegularExpression(@"^[a-zA-Z''-'\s]{1,50}$")]
         public string ClAddress { get; set; }
 
         [Required]
