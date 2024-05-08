367765c73095d54820fd6425bfb7decc10d2f3d6
hyldgaard01 <hyldgaardemilie@gmail.com>
2024-05-08 13:25:29 +0200

    UpdateMyPageClient

--- SemesterProjektDenAnden/SemesterProjektDenAnden/ClientFroms/MyPage.cs
+++ SemesterProjektDenAnden/SemesterProjektDenAnden/ClientFroms/MyPage.cs
@@ -3,9 +3,11 @@ using Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,16 +18,20 @@ namespace SemesterProjektDenAnden.ClientFroms
     {
         ClientBL clientBL;
         Client client;
+        ClientMDI clientMdi;
+        int clientId;
         public MyPage(ClientMDI clientMdi, int clientId)
         {
             InitializeComponent();
 
             clientBL = new ClientBL();
             GetClientData(clientId);
+            this.clientMdi = clientMdi;
+            this.clientId = clientId;
 
         }
 
-        public async void GetClientData(int clientId)
+        private async void GetClientData(int clientId)
         {
             client = await clientBL.GetAsync(clientId);
             FNameTxt.Text = client.FirstName;
@@ -33,14 +39,82 @@ namespace SemesterProjektDenAnden.ClientFroms
             MailTxt.Text = client.Mail;
             PhoneTxt.Text = client.Phone.ToString();
             AddressTxt.Text = client.ClAddress;
-            if(client.Subscriber)
+            if (client.Subscriber)
             {
                 SubscribtionStatusLbl.Text = "Abonnent";
+                SubscribeBtn.Text = "Opsig abonnement";
             }
             else
             {
                 SubscribtionStatusLbl.Text = "Ikke abonnent";
+                SubscribeBtn.Text = "Start abonnement";
             }
+        }
+
+        private async void safeChangesbtn_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("Er du sikker på du vil opdatere denne klients oplysnigner?", "Opdater klient", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+
+
+                Client updateClient = await clientBL.GetAsync(clientId);
+                updateClient.FirstName = FNameTxt.Text;
+                updateClient.LastName = LNameTxt.Text;
+                updateClient.Phone = int.Parse(PhoneTxt.Text);
+                updateClient.Mail = MailTxt.Text;
+                updateClient.ClAddress = AddressTxt.Text;
+                var context = new ValidationContext(updateClient, serviceProvider: null, items: null);
+                bool isValid = Validator.TryValidateObject(updateClient, context, null, true);
+
+                if (isValid)
+                {
+                    bool createResult = await clientBL.UpdateAsync(updateClient);
+                    if (createResult)
+                    {
+                        MessageBox.Show("Klient opdateret", "Klient opdateret");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Fejl: Klient ikke opdateret", "Fejl");
+                    }
+                }
+                else { MessageBox.Show("Fejl: Klient ikke opdateret", " info ikke valid"); };
+            }
+
+        }
+
+        private async void SubscribeBtn_Click(object sender, EventArgs e)
+        {
+            
+            Client updateClient = await clientBL.GetAsync(clientId);
+            if (updateClient.Subscriber)
+            {
+                DialogResult result = MessageBox.Show("Er du sikker på du vil opsige dit abonnement?", "Opsig Abonnement", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    updateClient.Subscriber = false;
+                    await clientBL.UpdateAsync(updateClient);
+                    MyPage newMyPage = new MyPage(clientMdi, clientId);
+                    clientMdi.FormOpener(newMyPage);
+                    
+                }
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Velkommen til LawHouse!\nTryk ja for at acceptere dit abonnement?", "Start Abonnement", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    updateClient.Subscriber = true;
+                    await clientBL.UpdateAsync(updateClient);
+                    MyPage newMyPage = new MyPage(clientMdi, clientId);
+                    clientMdi.FormOpener(newMyPage);
+                }
+            }
+
+
         }
     }
 }
