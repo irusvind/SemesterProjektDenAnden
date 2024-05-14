e9a9eeb9e931c2d3da2d87daa24b1e895f4d45cb
irusvind <irus@live.dk>
2024-05-14 21:05:08 +0200

    calculator

--- SemesterProjektDenAnden/SemesterProjektDenAnden/Tools/Calculator.cs
+++ SemesterProjektDenAnden/SemesterProjektDenAnden/Tools/Calculator.cs
@@ -20,6 +20,28 @@ namespace SemesterProjektDenAnden.Tools
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int G = int.Parse(sizeBox.Text);
+            double r = Convert.ToDouble(rateBox.Text)/100;
+            int perYear = int.Parse(paymentPYearBox.Text);
+            int amountYear = int.Parse(yearsBox.Text);
+            double n = perYear * amountYear;
+
+            double amountpyear = 0;
+
+            double temp;
+            double temp2 = (1 + r);
+
+            //temp = G * r / (1 - (1 + r));
+
+            temp = Math.Pow(temp2,  n);
+
+            amountpyear = G * (r / (1+temp));
+
+            paymentAmount.Text = amountpyear.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
 
         }
     }
